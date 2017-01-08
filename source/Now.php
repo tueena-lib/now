415410b4f0b69951f633c6a02277bb4f064bee0b
Bastian Fenske <info@bastian-fenske.eu>
2017-01-08 01:14:33 +0100

    Initial commit.

---
+++ source/Now.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Part of the tueena lib
+ *
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link http://tueena.org/
+ * @author Bastian Fenske <bastian.fenske@tueena.org>
+ * @file
+ */
+
+declare(strict_types=1);
+namespace tueenaLib\now;
+
+class Now implements INow
+{
+	public function __invoke(): \DateTimeImmutable
+	{
+		return new \DateTimeImmutable;
+	}
+}
---
+++ spec/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Part of the tueena lib
+ *
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link http://tueena.org/
+ * @author Bastian Fenske <bastian.fenske@tueena.org>
+ * @file
+ */
+
+declare(strict_types=1);
+spl_autoload_register(function ($className) {
+
+	$namespaceParts = explode('\\', $className);
+	$firstNamespacePart = array_shift($namespaceParts);
+	$secondNamespacePart = array_shift($namespaceParts);
+	if ($firstNamespacePart !== 'tueenaLib' || $secondNamespacePart !== 'now')
+		return false;
+
+	$basePath = __DIR__ . '/../';
+	if ($namespaceParts[0] !== 'spec')
+		$basePath .= 'source/';
+	$path = $basePath . implode('/', $namespaceParts) . '.php';
+	if (!is_readable($path))
+		return false;
+
+	include $path;
+	return true;
+});
